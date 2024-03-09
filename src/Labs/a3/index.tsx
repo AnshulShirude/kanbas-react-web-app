2cc57f030fcbc9db712db8863d926d156ee7d10a
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2024-03-08 19:20:58 -0500

    finished the lab - check add reducer thing working

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -7,8 +7,12 @@ import Highlight from "./Highlight";
 import Add from "./Add";
 import TodoList from "./todos/TodoList";
 import TodoItem from "./todos/TodoItem";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
+
 
 function Assignment3() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
   return (
     <div className="container">
       <h1>Assignment 3</h1>
@@ -24,6 +28,13 @@ function Assignment3() {
         vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
      </Highlight>
      <Add a={3} b={4} />
+     <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
